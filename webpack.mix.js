a52c2273afb125e9e2ce966d1420cc34ceea36da
ale395 <alexis.fernandez.rc@gmail.com>
2018-03-22 18:22:55 -0300

    Webpack-ejemplo

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,10 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
+mix.styles([
+	//aqui van las rutas de los css necesarios
+	], 'public/css');
+
 mix.scripts([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js'
